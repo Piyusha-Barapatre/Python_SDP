b6eb8eb43bf415f2181cb32daabbd79f9efa3b9a
Piyusha-Barapatre <85167734+Piyusha-Barapatre@users.noreply.github.com>
2021-08-30 20:04:56 +0530

    Update Swappingwith.py

--- Swappingwith.py
+++ Swappingwith.py
@@ -1,9 +1,11 @@
 x = input('Enter value of x: ')
 y = input('Enter value of y: ')
-
+print ("Before swapping: ")
+print("Value of x : ", x, " and y : ", y)
+ 
 temp = x
 x = y
 y = temp
-
-print('The value of x after swapping is ',x)
-print('The value of y after swapping is ',y)
+ 
+print ("After swapping: ")
+print("Value of x : ", x, " and y : ", y)
