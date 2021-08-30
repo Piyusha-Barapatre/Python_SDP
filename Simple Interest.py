01952fab5a27c00935bcf0b3a0c19f6d2b173bb7
Piyusha-Barapatre <85167734+Piyusha-Barapatre@users.noreply.github.com>
2021-08-30 19:48:37 +0530

    Create Simple Interest.py

---
+++ Simple Interest.py
@@ -0,0 +1,5 @@
+p =int(input("Enter the principal amount: "))
+t =int(input("Enter the time period: "))
+r =int(input("Enter the rate of interest: "))
+si = (p*t*r)/100;
+print('Simple intrest is Rs.',si);
