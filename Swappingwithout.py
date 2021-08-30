f7cf317e009050b12c67287ee6a67c518f0b0c52
Piyusha-Barapatre <85167734+Piyusha-Barapatre@users.noreply.github.com>
2021-08-30 20:03:50 +0530

    Create Swappingwithout.py

---
+++ Swappingwithout.py
@@ -0,0 +1,9 @@
+x = input('Enter value of x: ')
+y = input('Enter value of y: ')
+print ("Before swapping: ")
+print("Value of x : ", x, " and y : ", y)
+ 
+x, y = y, x
+ 
+print ("After swapping: ")
+print("Value of x : ", x, " and y : ", y)
